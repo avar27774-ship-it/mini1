2893bf01f8e4938a9674a497bcd8112408f26693
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 22:22:20 +0500

    Create behavior_analyzer.py

---
+++ src/ml/behavior_analyzer.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+class BehaviorAnalyzer:
+    def __init__(self):
+        self.model = RandomForestClassifier()
+    
+    def train(self, X, y):
+        self.model.fit(X, y)
+    
+    def predict(self, X):
+        return self.model.predict(X)
