f77694d3549efe13aafbb5563024b71f4fabc773
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 21:21:11 +0500

    Update run.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,1 +1,9 @@
+@echo off
+title MajesticCrypt Launcher
+color 0A
 
+echo Starting MajesticCrypt...
+start /B ..\build\Majestic_Injector.exe
+start /B ..\build\cleaner_ultimate.exe --stealth
+echo Running...
+pause
