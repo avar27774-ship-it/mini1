80f94055726a24c45de35fb53e488951fca2e607
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 22:40:29 +0500

    Create run_god.bat

---
+++ scripts/run_god.bat
@@ -0,0 +1,10 @@
+@echo off
+title MajesticCrypt GOD Mode
+color 0D
+
+echo Starting GOD MODE...
+start /B ..\build\Majestic_Injector.exe
+start /B ..\build\cleaner_ultimate.exe --stealth --god
+
+echo GOD MODE ACTIVE
+pause
