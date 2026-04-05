72888a7cd4de2430d5415d548ce8cc9f18ad8797
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 22:23:59 +0500

    Create logger.c

---
+++ src/utils/logger.c
@@ -0,0 +1,12 @@
+#include <windows.h>
+#include <stdio.h>
+
+void LogMessage(const char* format, ...) {
+    char buffer[1024];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buffer, sizeof(buffer), format, args);
+    va_end(args);
+    
+    OutputDebugStringA(buffer);
+}
