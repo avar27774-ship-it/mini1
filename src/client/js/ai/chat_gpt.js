a2716a61a837e6c9fbe3a8686f4a8a9bdb60a5f3
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 21:53:12 +0500

    Create chat_gpt.js

---
+++ src/client/js/ai/chat_gpt.js
@@ -0,0 +1,10 @@
+export default class ChatGPT {
+    constructor() {
+        this.apiKey = "";
+    }
+    
+    async generate(prompt) {
+        // Интеграция с ChatGPT API
+        return "Response from AI";
+    }
+}
