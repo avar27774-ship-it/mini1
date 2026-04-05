cfd291956fbc11956d5f1a02757012e4453b0b15
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 21:09:22 +0500

    Update visual_god.js

--- src/client/js/god/visual_god.js
+++ src/client/js/god/visual_god.js
@@ -1,1 +1,44 @@
+import * as alt from 'alt-client';
+import * as native from 'natives';
 
+export default class VisualGod {
+    constructor() {
+        this.matrixActive = false;
+    }
+    
+    setCyberpunk() {
+        native.setTimecycleModifier("cyc_glass");
+        native.setExtraTimecycleModifier("neon");
+    }
+    
+    setVaporwave() {
+        native.setTimecycleModifier("vaporwave");
+    }
+    
+    setMatrix() {
+        this.matrixActive = true;
+        native.setTimecycleModifier("matrix");
+        
+        setInterval(() => {
+            if (!this.matrixActive) return;
+            const pos = alt.Player.local.pos;
+            for (let i = 0; i < 20; i++) {
+                native.addTextEntry("MATRIX", Math.floor(Math.random() * 2).toString());
+                native.drawText(pos.x + (Math.random() - 0.5) * 10,
+                               pos.y + (Math.random() - 0.5) * 10,
+                               pos.z + Math.random() * 5);
+            }
+        }, 100);
+    }
+    
+    stopMatrix() {
+        this.matrixActive = false;
+        native.setTimecycleModifier("DEFAULT");
+    }
+    
+    whiteOut() {
+        native.setTimecycleModifier("WHITE");
+        native.triggerScreenblurFadeIn(500);
+        setTimeout(() => native.triggerScreenblurFadeOut(500), 3000);
+    }
+}
