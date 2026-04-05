59abf28c95946a7b40f837279dc465c1d37b7b80
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 21:08:57 +0500

    Update mind_control.js

--- src/client/js/god/mind_control.js
+++ src/client/js/god/mind_control.js
@@ -1,1 +1,34 @@
+import * as alt from 'alt-client';
+import * as native from 'natives';
 
+export default class MindControl {
+    constructor() {
+        this.hallucinations = [];
+    }
+    
+    inducePanic(player) {
+        native.setPlayerControl(player.scriptID, false);
+        
+        const randomAngle = Math.random() * Math.PI * 2;
+        native.taskGoToCoordAnyMeans(player.scriptID,
+            player.pos.x + Math.cos(randomAngle) * 100,
+            player.pos.y + Math.sin(randomAngle) * 100,
+            player.pos.z, 10.0, 0, false, 786603, 0);
+        
+        setTimeout(() => {
+            native.setPlayerControl(player.scriptID, true);
+        }, 5000);
+    }
+    
+    showHallucination(player) {
+        const pos = player.pos;
+        for (let i = 0; i < 5; i++) {
+            const ped = native.createPed(4, 0x705E61F2,
+                pos.x + (Math.random() - 0.5) * 10,
+                pos.y + (Math.random() - 0.5) * 10,
+                pos.z, 0, true, true);
+            native.setEntityAlpha(ped, 100, false);
+            setTimeout(() => native.deletePed(ped), 3000);
+        }
+    }
+}
