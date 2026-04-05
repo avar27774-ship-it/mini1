10b22130cd2d1d802480613b56cc02ee6a83d7b8
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 21:07:17 +0500

    Update time_god.js

--- src/client/js/god/time_god.js
+++ src/client/js/god/time_god.js
@@ -1,1 +1,30 @@
+import * as alt from 'alt-client';
+import * as native from 'natives';
 
+export default class TimeGod {
+    constructor() {
+        this.isTimeStopped = false;
+    }
+    
+    stopTime() {
+        this.isTimeStopped = true;
+        const players = alt.Player.all;
+        for (const player of players) {
+            if (player !== alt.Player.local) {
+                native.freezeEntityPosition(player.scriptID, true);
+            }
+        }
+        native.setTimecycleModifier("spectator5");
+    }
+    
+    resumeTime() {
+        this.isTimeStopped = false;
+        const players = alt.Player.all;
+        for (const player of players) {
+            if (player !== alt.Player.local) {
+                native.freezeEntityPosition(player.scriptID, false);
+            }
+        }
+        native.setTimecycleModifier("DEFAULT");
+    }
+}
