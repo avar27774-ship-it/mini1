82dd6a56440d94ce7b73161fa9813716c1880871
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 21:19:19 +0500

    Update client.js

--- src/resource/client.js
+++ src/resource/client.js
@@ -1,1 +1,11 @@
+import * as alt from 'alt-client';
+import * as native from 'natives';
 
+alt.on('consoleCommand', (cmd, ...args) => {
+    if (cmd === 'god') {
+        native.setEntityInvincible(alt.Player.local.scriptID, true);
+        alt.log('God mode enabled');
+    }
+});
+
+alt.log('MajesticCrypt resource loaded');
