7a4a241f1ec8bf5f6e474c3c9fcf0b9013800cc9
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 21:18:40 +0500

    Update cleaner_ultimate.c

--- src/cleaner/cleaner_ultimate.c
+++ src/cleaner/cleaner_ultimate.c
@@ -1,1 +1,34 @@
+#include <windows.h>
+#include <stdio.h>
 
+void CleanDirectory(const char* path) {
+    char cmd[512];
+    snprintf(cmd, sizeof(cmd), "rmdir /s /q \"%s\" 2>nul", path);
+    system(cmd);
+}
+
+void CleanRegistry(const char* key) {
+    char cmd[512];
+    snprintf(cmd, sizeof(cmd), "reg delete \"%s\" /f 2>nul", key);
+    system(cmd);
+}
+
+int main() {
+    printf("MajesticCleaner Ultimate\n");
+    
+    CleanDirectory("%LocalAppData%\\altv-majestic\\logs");
+    CleanDirectory("%LocalAppData%\\altv-majestic\\cache");
+    CleanDirectory("%AppData%\\ALTv\\logs");
+    CleanDirectory("%Temp%\\altv*");
+    CleanDirectory("%ProgramData%\\EasyAntiCheat\\Logs");
+    
+    CleanRegistry("HKCU\\Software\\ALTv");
+    CleanRegistry("HKCU\\Software\\Majestic");
+    
+    system("ipconfig /flushdns");
+    system("wevtutil cl Application");
+    system("wevtutil cl System");
+    
+    printf("Cleaning complete!\n");
+    return 0;
+}
