b0c2ebeb56f1fbd76bd911e74553d77edf60b0dd
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 21:21:28 +0500

    Update clean.bat

--- scripts/clean.bat
+++ scripts/clean.bat
@@ -1,1 +1,11 @@
+@echo off
+title MajesticCrypt Cleaner
+color 0C
 
+echo Cleaning MajesticCrypt traces...
+sc stop MajesticGod
+sc delete MajesticGod
+del /f /q %SystemRoot%\System32\drivers\majestic_driver.sys 2>nul
+rmdir /s /q "%LocalAppData%\altv-majestic\resources\majestic_god" 2>nul
+echo Clean complete!
+pause
