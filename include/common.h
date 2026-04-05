7027ff2e696edbe099c5d17d9447adeed08674c4
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 22:27:27 +0500

    Create common.h

---
+++ include/common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAJESTIC_VERSION "4.0.0"
+#define MAJESTIC_NAME "MajesticCrypt GOD Edition"
+
+typedef struct Vector3 {
+    float x, y, z;
+} Vector3;
