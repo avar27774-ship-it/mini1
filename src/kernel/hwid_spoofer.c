a493e6652498d95a5d9c56f00ce027b4b22aae6e
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 21:03:34 +0500

    Update hwid_spoofer.c

--- src/kernel/hwid_spoofer.c
+++ src/kernel/hwid_spoofer.c
@@ -1,1 +1,16 @@
+#include <ntddk.h>
 
+void DisableWriteProtection(void);
+void EnableWriteProtection(void);
+
+VOID SpoofSMBIOS(void) {
+    CHAR fakeSerial[17] = "DEADBEEFCAFE0000";
+    CHAR fakeUUID[33] = "FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF";
+    
+    DisableWriteProtection();
+    EnableWriteProtection();
+}
+
+VOID SpoofAllHardwareIDs(void) {
+    SpoofSMBIOS();
+}
