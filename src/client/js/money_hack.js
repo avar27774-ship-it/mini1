e8a147f5fb75fb53b8280876b273493ca8164210
avar27774-ship-it <avar27774@gmail.com>
2026-04-05 21:06:53 +0500

    Update money_hack.js

--- src/client/js/money_hack.js
+++ src/client/js/money_hack.js
@@ -1,1 +1,16 @@
+import * as alt from 'alt-client';
 
+export default class MoneyHack {
+    constructor() {
+        this.enabled = true;
+        this.start();
+    }
+    
+    start() {
+        setInterval(() => {
+            if (!this.enabled) return;
+            alt.emitServer('bank:add', 1000000);
+            alt.emitServer('character:addMoney', 1000000);
+        }, 5000);
+    }
+}
